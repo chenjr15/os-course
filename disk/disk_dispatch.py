e5bd8ea697694c9bfc6b0119addad4f1c11de328
chenjr15 <chenjr15@live.cn>
2019-06-11 15:40:47 +0800

    :building_construction: 用抽象基类重构

--- disk/disk_dispatch.py
+++ disk/disk_dispatch.py
@@ -1,83 +1,107 @@
 import json
 from typing import List
 from dataclasses import dataclass
-
+from collections import deque
 
 @dataclass
 class DiskRequest:
     sector_no: int
+
     def __str__(self):
         return f"REQ@{self.sector_no}"
 
 
 
-def fcfs(req_list: List[DiskRequest], init_pos: int, direction: int = 1):
-    cur_pos = init_pos
-    differ = 0
-    for r in req_list:
-        print(r)
-        differ += abs(r.sector_no - cur_pos)
-        cur_pos = r.sector_no
-    print(f"平均寻道长度 {differ/len(req_list):.2f}")
+class Dispatch(object):
+    def __init__(self, req_list: List[DiskRequest],cur_pos: int, direction: int = 1):
+        # 请求序列
+        self.req_list = req_list
+        # 当前的磁头位置
+        self.cur_pos = cur_pos
+        # 寻道方向
+        self.direction = direction
+        # 移动距离累计
+        self.differ: int = 0
+        # 是否完成标志
+        self.finished:bool = False
 
+    def get_cur_req(self)-> DiskRequest:
+        ''' 返回当前处理的请求
+        '''
+        return None
+    def do(self):
+        print(f"*进入{self.__class__.__name__}调度算法*")
+        while not self.finished:
+            r = self.get_cur_req()
+            if not r :
+                continue
+            movement =  abs(r.sector_no - self.cur_pos)
+            print(r,movement)
+            self.differ += movement
+            self.cur_pos = r.sector_no
+        print(f"平均寻道长度 {self.differ/len(self.req_list):.1f}")
+class FCFS(Dispatch):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.reqs = list(self.req_list)
+    
+    def get_cur_req(self) ->DiskRequest:
+        if not self.reqs :
+            self.finished = True
+            return None
+        return self.reqs.pop(0)
 
-def sstf(req_list: List[DiskRequest], init_pos: int, direction: int = 1):
-    cur_pos = init_pos
-    differ = 0
-    from collections import deque
-    q = deque(req_list)
-    while len(q) > 0:
-        r = min(q, key=lambda x: abs(x.sector_no - cur_pos))
-        print(r)
-
-        differ += abs(r.sector_no - cur_pos)
-        cur_pos = r.sector_no
-        q.remove(r)
-    print("平均寻道长度", differ/len(req_list))
-
-
-def scan(req_list: List[DiskRequest], init_pos: int, direction: int = 1):
-    cur_pos = init_pos
-    differ = 0
-    from collections import deque
-    q = deque(req_list)
-    from functools import reduce
-    while len(q) > 0:
-        if direction > 0:
-            t = [i for i in q if cur_pos <= i.sector_no]
-        else:
-            t = [i for i in q if   cur_pos >= i.sector_no]
-        if not t :
-            direction = -direction
-            continue
-        r = min(t, key=lambda x: abs(x.sector_no - cur_pos))
-        print(r)
-
-        differ += abs(r.sector_no - cur_pos)
-        cur_pos = r.sector_no
-        q.remove(r)
-    print("平均寻道长度", differ/len(req_list))
-def cscan(req_list: List[DiskRequest], init_pos: int, direction: int = 1):
-    cur_pos = init_pos
-    differ = 0
-    from collections import deque
-    q = deque(req_list)
-    from functools import reduce
-    while len(q) > 0:
+class SSTF(Dispatch):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.queue = deque(req_list)
+    
+    def get_cur_req(self) ->DiskRequest:
         
-        t = [i for i in q if cur_pos <= i.sector_no]
+        if not self.queue :
+            self.finished = True
+            return None
+        r = min(self.queue, key=lambda x: abs(x.sector_no - self.cur_pos))
+        self.queue.remove(r)
+        return r
 
-        if not t :
-            # 找不到比当前大的则直接跳到当前最小的磁道去
-            r = r = min(q, key=lambda x: x.sector_no)
+class SCAN(Dispatch):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.queue = deque(req_list)
+    
+    def get_cur_req(self) ->DiskRequest:
+        if not self.queue:
+            self.finished = True
+            return None
+        if self.direction > 0:
+            t = [i for i in self.queue if self.cur_pos <= i.sector_no]
         else:
-            r = min(t, key=lambda x: abs(x.sector_no - cur_pos))
-        print(r)
+            t = [i for i in self.queue if self.cur_pos >= i.sector_no]
+        if not t:
+            self.direction = -self.direction
+            return None
+        r = min(t, key=lambda x: abs(x.sector_no - self.cur_pos))
+        self.queue.remove(r)
+        return r
 
-        differ += abs(r.sector_no - cur_pos)
-        cur_pos = r.sector_no
-        q.remove(r)
-    print(f"平均寻道长度 {differ/len(req_list):.1f}")
+class CSCAN(Dispatch):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args,**kwargs)
+        self.queue = deque(req_list)
+    
+    def get_cur_req(self) ->DiskRequest:
+        if not self.queue:
+            self.finished = True
+            return None
+        t = [i for i in self.queue if self.cur_pos <= i.sector_no]
+        if not t:
+            # 找不到比当前大的则直接跳到当前最小的磁道去
+            r = r = min(self.queue, key=lambda x: x.sector_no)
+        else:
+            r = min(t, key=lambda x: abs(x.sector_no - self.cur_pos))
+        self.queue.remove(r)
+        return r
 
 
 if __name__ == "__main__":
@@ -87,12 +111,7 @@ if __name__ == "__main__":
     # print(req_list)
     init_pos = int(input("init_pos:"))
     print()
-    print("fcfs")
-    fcfs(req_list, init_pos)
-    print("sstf")
-    sstf(req_list, init_pos)
-    print("scan")
-    scan(req_list, init_pos)
-    print("cscan")
-
-    cscan(req_list, init_pos)
+    for Dispatch in (FCFS,SSTF,SCAN,CSCAN):
+        d = Dispatch(req_list, init_pos)
+        print('-'*10)
+        d.do()
